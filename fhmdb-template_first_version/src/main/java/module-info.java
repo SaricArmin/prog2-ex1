747779969a121191549965db77f86d83eee5ef81
harun <harun.dogan@stud.fh-campuswien.ac.at>
2023-03-12 20:25:27 +0100

    fixed order structure

--- fhmdb-template_first_version/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module at.ac.fhcampuswien.fhmdb {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.jfoenix;
-
-    opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
-    exports at.ac.fhcampuswien.fhmdb;
-}
